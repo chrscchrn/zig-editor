9a98b91e3b150560020a4d416bb1717cd58f7380
Christopher Cochran <cochranschris@gmail.com>
2024-10-17 22:55:52 -0500

    comments bc why not

--- src/main.zig
+++ src/main.zig
@@ -10,25 +10,25 @@ fn logGLFWError(error_code: glfw.ErrorCode, description: [:0]const u8) void {
 const print = std.debug.print;
 
 pub fn main() !void {
+    // glfw
     glfw.setErrorCallback(logGLFWError);
-    // GLFW setup
     if (!glfw.init(.{})) {
         glfw_log.err("failed to initialize GLFW: {?s}", .{glfw.getErrorString()});
         return error.GLFWInitFailed;
     }
     defer glfw.terminate();
-
     const window = glfw.Window.create(1920, 1080, "editor made with zig", null, null, .{});
     defer window.?.destroy();
-
     glfw.makeContextCurrent(window);
     defer glfw.makeContextCurrent(null);
 
+    // arg input and open file to buff
     const fileName = try processArg();
     const allocator = std.heap.page_allocator;
     const buff = try readFile(allocator, fileName);
     defer allocator.free(buff);
     print("{s}", .{buff});
+
     main_loop: while (true) {
         glfw.waitEvents();
         if (window.?.shouldClose()) break :main_loop;
