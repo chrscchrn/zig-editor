b5a4bd3764c86ae59f618d6a676d2032fe3f14ff
Christopher Cochran <cochranschris@gmail.com>
2024-10-19 12:15:06 -0500

    creating shaders, program

--- src/main.zig
+++ src/main.zig
@@ -6,9 +6,15 @@ const harfbuzz = @import("mach-harfbuzz");
 const fonts = @import("zig-fonts");
 
 const glfw_log = std.log.scoped(.glfw);
+const gl_log = std.log.scoped(.gl);
+const ft_log = std.log.scoped(.ft);
 
 const screenWidth = 1920;
 const screenHeight = 1080;
+// const screenSize = Size{
+//     .x = 1920,
+//     .y = 1080,
+// };
 
 const print = std.debug.print;
 
@@ -53,6 +59,36 @@ const Bearing = struct {
     x: i32,
     y: i32,
 };
+
+// OpenGL Shaders
+const vertex_shader_source: [:0]const u8 =
+    \\#version 330 core
+    \\layout (location = 0) in vec4 vertex; // <vec2 pos, vec2 tex>
+    \\out vec2 TexCoords;
+    \\
+    \\uniform mat4 projection;
+    \\
+    \\void main()
+    \\{
+    \\    gl_Position = projection * vec4(vertex.xy, 0.0, 1.0);
+    \\    TexCoords = vertex.zw;
+    \\}
+;
+
+const fragment_shader_source: [:0]const u8 =
+    \\#version 330 core
+    \\in vec2 TexCoords;
+    \\out vec4 color;
+    \\
+    \\uniform sampler2D text;
+    \\uniform vec3 textColor;
+    \\
+    \\void main()
+    \\{
+    \\    vec4 sampled = vec4(1.0, 1.0, 1.0, texture(text, TexCoords).r);
+    \\    color = vec4(textColor, 1.0) * sampled;
+    \\}
+;
 
 pub fn main() !void {
     // glfw
@@ -141,16 +177,80 @@ pub fn main() !void {
     }
     gl.binding.pixelStorei(gl.binding.UNPACK_ALIGNMENT, 1);
 
-    // Shaders
+    gl.binding.enable(gl.binding.BLEND);
+    gl.binding.blendFunc(gl.binding.SRC_ALPHA, gl.binding.ONE_MINUS_SRC_ALPHA);
+
+    // Create GL Program
+    const program = create_program: {
+        var success: c_uint = undefined;
+        var info_log_buf: [512:0]u8 = undefined;
+
+        const vertex_shader = gl.binding.createShader(gl.binding.VERTEX_SHADER);
+        if (vertex_shader == 0) return error.CreateVertexShaderFailed;
+        defer gl.binding.deleteShader(vertex_shader);
+
+        gl.binding.shaderSource(
+            vertex_shader,
+            1,
+            (&vertex_shader_source.ptr)[0..1],
+            &@as(c_int, @intCast(vertex_shader_source.len)),
+        );
+        gl.binding.compileShader(vertex_shader);
+        gl.binding.getShaderiv(vertex_shader, gl.binding.COMPILE_STATUS, @ptrCast(&success));
+        if (success == gl.binding.FALSE) {
+            gl.binding.getShaderInfoLog(vertex_shader, info_log_buf.len, null, &info_log_buf);
+            gl_log.err("{s}", .{std.mem.sliceTo(&info_log_buf, 0)});
+            return error.CompileVertexShaderFailed;
+        }
+
+        const fragment_shader = gl.binding.createShader(gl.binding.FRAGMENT_SHADER);
+        if (fragment_shader == gl.binding.FALSE) return error.CreateFragmentShaderFailed;
+        defer gl.binding.deleteShader(fragment_shader);
+
+        gl.binding.shaderSource(
+            fragment_shader,
+            1,
+            (&fragment_shader_source.ptr)[0..1],
+            &@as(c_int, @intCast(fragment_shader_source.len)),
+        );
+        gl.binding.compileShader(fragment_shader);
+        gl.binding.getShaderiv(fragment_shader, gl.binding.COMPILE_STATUS, @ptrCast(&success));
+        if (success == gl.binding.FALSE) {
+            gl.binding.getShaderInfoLog(fragment_shader, info_log_buf.len, null, &info_log_buf);
+            gl_log.err("{s}", .{std.mem.sliceTo(&info_log_buf, 0)});
+            return error.CompileFragmentShaderFailed;
+        }
+
+        const program = gl.binding.createProgram();
+        if (program == 0) return error.CreateProgramFailed;
+        errdefer gl.binding.deleteProgram(program);
+
+        gl.binding.attachShader(program, vertex_shader);
+        gl.binding.attachShader(program, fragment_shader);
+        gl.binding.linkProgram(program);
+        gl.binding.getProgramiv(program, gl.binding.LINK_STATUS, @ptrCast(&success));
+        if (success == gl.binding.FALSE) {
+            gl.binding.getProgramInfoLog(program, info_log_buf.len, null, &info_log_buf);
+            gl_log.err("{s}", .{std.mem.sliceTo(&info_log_buf, 0)});
+            return error.LinkProgramFailed;
+        }
+
+        break :create_program program;
+    };
+    defer gl.binding.deleteProgram(program);
 
     main_loop: while (true) {
         glfw.waitEvents();
         if (window.shouldClose()) break :main_loop;
 
+        gl.binding.clearColor(1, 0, 1, 1);
+        gl.binding.clear(gl.binding.COLOR_BUFFER_BIT);
+
         window.swapBuffers();
     }
 }
 
+// Helper Functions
 fn processArg() ![]const u8 {
     var argIter = std.process.args();
     _ = argIter.next();
